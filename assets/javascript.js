9b926f0cff65c83d35f27f3b59d6a03a5936c5a2
Samy <sraman@berkeley.edu>
2018-01-12 08:59:36 -0800

    fixed image sizes

--- assets/javascript.js
+++ assets/javascript.js
@@ -4,3 +4,18 @@ $("a").on('click', function(event) {
   }, 500);
   return false;
 });
+$(".tile img").on('click', function(event) {
+	console.log("img clicked");
+	$(this).siblings(".modal").addClass("is-active");
+});
+$("button.modal-close").on('click', function(event) {
+	$(this).parent(".modal").removeClass("is-active");
+});
+$(".modal-background").on('click', function(event) {
+	$(this).parent(".modal").removeClass("is-active");
+});
+$('body').keydown(function(e) {
+    if (e.keyCode == 27) {
+        $(".modal").removeClass("is-active");
+    }
+});
