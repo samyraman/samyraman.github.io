435e47fdfcecc6c820827a853a16818ab5605554
samyraman <sraman@berkeley.edu>
2018-03-10 20:46:11 -0800

    Update connectfour.js

--- ConnectFour/connectfour.js
+++ ConnectFour/connectfour.js
@@ -123,8 +123,14 @@ function scroll() {
         var x = parseInt(touched.clientX) - document.getElementById("gameBoard").offsetLeft;
 	console.log(x);
         var width = document.querySelector(".circle").offsetWidth;
-	    var scrollPos = ~~(x/30);
-	    dropDisc(currPlayer, scrollPos);
+	var scrollPos = ~~(x/30);
+	if (scrollPos >= 6) {
+		scrollPos = 6;
+	}
+	if (scrollPos <= 0) {
+		scrollPos = 0;
+	}
+	dropDisc(currPlayer, scrollPos);
     }, false)
 };
 
